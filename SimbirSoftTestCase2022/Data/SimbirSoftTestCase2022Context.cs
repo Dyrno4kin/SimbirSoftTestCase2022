41f503ef47325e1c44e2da67e9ea21452e57731a
A Dyrkin <adresat277@gmail.com>
2022-01-25 22:54:25 +0400

    Добавьте файлы проекта.

---
+++ SimbirSoftTestCase2022/Data/SimbirSoftTestCase2022Context.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SimbirSoftTestCase2022.Models;
+
+namespace SimbirSoftTestCase2022.Data
+{
+    public class SimbirSoftTestCase2022Context : DbContext
+    {
+        public SimbirSoftTestCase2022Context (DbContextOptions<SimbirSoftTestCase2022Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SimbirSoftTestCase2022.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ SimbirSoftTestCase2022/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SimbirSoftTestCase2022.Data;
+using SimbirSoftTestCase2022.Models;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<SimbirSoftTestCase2022Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SimbirSoftTestCase2022Context")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
+var defaultCulture = new CultureInfo("es-UY");
+var localizationOptions = new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture(defaultCulture),
+    SupportedCultures = new List<CultureInfo> { defaultCulture },
+    SupportedUICultures = new List<CultureInfo> { defaultCulture }
+};
+app.UseRequestLocalization(localizationOptions);
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
